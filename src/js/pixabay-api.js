150c210f286b1f28f84adbc177374b261a249509
yevgen-gumma <203040eg@gmail.com>
2024-02-11 14:43:14 +0200

    2

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,4 +1,4 @@
 // Описаний у документації
-import iziToast from "izitoast";
+import iziToast from 'izitoast';
 // Додатковий імпорт стилів
-import "izitoast/dist/css/iziToast.min.css";
+import 'izitoast/dist/css/iziToast.min.css';
